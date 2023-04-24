757edee2c304dbbd690f97e4ae42b20c438ddc27
Alex Frank <alexfrankcodes@gmail.com>
2023-04-24 16:18:44 -0700

    chore: Modifying layout styling to fit full screen and adding footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,31 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Meal Mixer',
-  description: 'A website to help you cook some delicious meals.',
-}
+  title: "Meal Mixer",
+  description: "A website to help you cook some delicious meals.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} min-h-screen flex flex-col items-center justify-between`}
+      >
+        {children}
+        <footer className="text-center">
+          <div className="flex justify-center">
+            <hr className="w-80" />
+          </div>
+          <p className="p-2">&copy; 2023 Alex Frank</p>
+        </footer>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import Image from 'next/image'
-import { Pacifico } from 'next/font/google'
+import Image from "next/image";
+import { Pacifico } from "next/font/google";
 
 const pacifico = Pacifico({
-  weight: '400',
-  subsets: ['latin'],
+  weight: "400",
+  subsets: ["latin"],
 });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className={`${pacifico.className} text-8xl`}>Meal Mixer</h1>
+    <main>
+      <h1 className={`${pacifico.className} text-7xl pt-20`}>Meal Mixer</h1>
     </main>
-  )
+  );
 }
