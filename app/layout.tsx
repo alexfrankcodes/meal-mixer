edaf4610dcdc4a4c54f2e3dd8159846fe4b6450b
Alex Frank <alexfrankcodes@gmail.com>
2023-04-27 17:46:55 -0700

    fix: Moving footer to page.tsx to solve hydration issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,16 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="min-h-screen flex flex-col justify-between">
+    <html lang="en" className="h-screen">
       <body className={`${inter.className}`}>{children}</body>
-      <footer className="text-center text-gray-50">
-        <div>
-          <hr className="w-80 inline-block" />
-        </div>
-        <p className="p-4 font-medium from-neutral-50">
-          &copy; 2023 Alex Frank
-        </p>
-      </footer>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,17 @@ export default function Home() {
   return (
     <main>
       <h1
-        className={`${pacifico.className} text-7xl pt-20 w-full flex justify-center`}
+        className={`${pacifico.className} text-7xl pt-20 pb-10 w-full flex justify-center`}
       >
         Meal Mixer
       </h1>
       <IngredientList />
+      <footer className="text-center text-gray-50 pt-10">
+        <div className="flex justify-center">
+          <hr className="border-gray-50 w-80 " />
+        </div>
+        <p className="p-4 letter">&copy; 2023 Alex Frank</p>
+      </footer>
     </main>
   );
 }
