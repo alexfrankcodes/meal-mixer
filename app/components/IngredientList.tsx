ac0083d2019d856549097c287c9ebb00cd0b0a3a
Alex Frank <alexfrankcodes@gmail.com>
2023-04-27 17:46:26 -0700

    feat: Making ingredient list entries dynamic

--- app/components/IngredientList.tsx
+++ app/components/IngredientList.tsx
@@ -5,18 +5,25 @@ import { Ingredient } from "./Ingredient";
 import { Button } from "./Button";
 import { IngredientForm } from "./IngredientForm";
 
+const testIngredients = [
+  { name: "Chicken", amount: 1 },
+  { name: "Rice", amount: 2 },
+  { name: "Broccoli", amount: 3 },
+  { name: "Soy Sauce", amount: 4 },
+];
+
 export const IngredientList = () => {
-  const [ingredients, setIngredients] = useState([]);
+  const [ingredients, setIngredients] = useState(testIngredients);
 
   const addIngredient = () => {
     setIngredients([...ingredients]);
   };
 
   return (
-    <div className="mt-10 flex flex-col w-full items-center">
-      <Ingredient name="Chicken" />
-      <Ingredient name="Rice" />
-      <Ingredient name="Broccoli" amount={3} />
+    <div className="mt-1{0 flex flex-col w-full items-center">
+      {ingredients.map(({ name, amount = 1 }) => (
+        <Ingredient key={name} name={name} amount={amount} />
+      ))}
 
       <IngredientForm />
 
