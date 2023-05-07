e26ff5cecde82090452ea580bfaad580ddc39b96
Alex Frank <alexfrankcodes@gmail.com>
2023-05-07 10:32:40 -0700

    chore: Updating metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Meal Mixer",
   description: "A website to help you cook some delicious meals.",
+  authors: [{ name: "Alex Frank", url: "https://alexfrankcodes.com" }],
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  category: "Food & Drink",
+  keywords: ["food", "recipes", "ingredients", "cooking", "meal", "mixer"],
 };
 
 export default function RootLayout({
